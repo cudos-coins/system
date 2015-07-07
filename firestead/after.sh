18424171134b1058d3abb61a778d855963d541f5
bjoern.lange <bjoern.lange@pixolith.de>
2015-07-07 21:51:36 +0200

    + added db migrations and seeding to the local deployment * laravel update

--- firestead/after.sh
+++ firestead/after.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-
-# If you would like to do some extra provisioning you may
-# add any commands you wish to this file and they will
-# be run after the Homestead machine is provisioned.
+php /vagrant/artisan migrate;
+php /vagrant/artisan db:seed;
